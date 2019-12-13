2fc3b160894e3d38b77df39f9bb4e7929b13252c
abhinav7693ngh <abhinav7693ngh@gmail.com>
2019-12-14 03:19:28 +0530

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,108 +1,115 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Person from './Person/Person';
-import Validation from './Validation/Validation';
-import CharCompo from './Char/Char';
+import Person from './Person/Person';
+import styled from 'styled-components';
+
+
+const StyledButton = styled.button`
+             background-color : ${props => props.alt ? 'red' : 'green' };
+              color : white;
+              font : inherit;
+              border : 1px solid blue;
+              padding : 8px;
+              &:hover {
+                background-color : ${props => props.alt ? 'salmon' : 'lightgreen'};
+                color : black;
+              }
+`;
 
 
 class App extends Component {
-  state = {
-    textLength : 0,
-    mytext : ''
+  state  = {
+    person : [
+      {id : 1,name : 'Abhi', age : 21},
+      {id:  2,name : 'Second', age : 23},
+      {id: 3, name : 'third', age: 24}
+    ],
+    showPerson : false
   }
 
-  textChangeHandler = (event) =>{
-    const mylength = event.target.value.length;
-    const textchange = event.target.value;
+  deletePersonHandler =(personIndex) =>{
+    const persons = [...this.state.person];
+    persons.splice(personIndex,1);
+    this.setState({person : persons})
+  }
 
+  nameChangedHandler = (event , id) =>{
+    const personIndex = this.state.person.findIndex((p)=>{
+      return p.id === id;
+    })
+    const copyPersonObj = {
+      ...this.state.person[personIndex]
+    }
+    //const copyPersonObj = Object.assign({},this.state.person[personIndex]);
+    copyPersonObj.name = event.target.value;
+
+    const copyPersonArr = [...this.state.person];
+    
+    copyPersonArr[personIndex] = copyPersonObj;
     this.setState({
-      textLength : mylength,
-      mytext : textchange
+      person: copyPersonArr
     })
   }
-  // state  = {
-  //   person : [
-  //     {id : 1,name : 'Abhi', age : 21},
-  //     {id:  2,name : 'Second', age : 23}
-  //   ],
-  //   showPerson : false
-  // }
-
-  // deletePersonHandler =(personIndex) =>{
-  //   const persons = [...this.state.person];
-  //   persons.splice(personIndex,1);
-  //   this.setState({person : persons})
-  // }
-
-  // nameChangedHandler = (event , id) =>{
-  //   const personIndex = this.state.person.findIndex((p)=>{
-  //     return p.id === id;
-  //   })
-  //   const copyPersonObj = {
-  //     ...this.state.person[personIndex]
-  //   }
-  //   //const copyPersonObj = Object.assign({},this.state.person[personIndex]);
-  //   copyPersonObj.name = event.target.value;
-
-  //   const copyPersonArr = [...this.state.person];
-    
-  //   copyPersonArr[personIndex] = copyPersonObj;
-  //   this.setState({
-  //     person: copyPersonArr
-  //   })
-  // }
 
 
-  // togglePersonHandler = () =>{
-  //   const personShow = this.state.showPerson;
-  //   this.setState({showPerson : !personShow});
-  // }
+  togglePersonHandler = () =>{
+    const personShow = this.state.showPerson;
+    this.setState({showPerson : !personShow});
+  }
 
 
   render() {
     // const Style = {
-    //   backgroundColor : 'White',
+    //   backgroundColor : 'green',
+    //   color : 'white',
     //   font : 'inherit',
     //   border : '1px solid blue',
-    //   padding : '8px'
+    //   padding : '8px',
+    //   ':hover':{
+    //     backgroundColor :'lightgreen',
+    //     color : 'black'
+    //   }
     // }
 
-    // let persons = null;
-    // if (this.state.showPerson){
-    //   persons = (
-    //     <div>
-    //       {this.state.person.map((person,index) =>{
-    //         return <Person
-    //                   name={person.name}
-    //                   age={person.age}
-    //                   click = {() =>this.deletePersonHandler(index)}
-    //                   key ={person.id}
-    //                   changed = {(event) => this.nameChangedHandler(event,person.id)}
-    //                 />
-    //       })}
-    //     </div>
-    //   );
-    // }
-    let character = null;
-    const myarray = Array.from(this.state.mytext);
-
-    if(myarray.length>0){
-       character = (
-         <div>
-         {myarray.map(ele => {
-           return <CharCompo element={ele} textchanged={(event => this.textChangeHandler(event))}/>
-         })}
-         </div>
-       )
+    let persons = null;
+    if (this.state.showPerson){
+      persons = (
+        <div>
+          {this.state.person.map((person,index) =>{
+            return <Person
+                      name={person.name}
+                      age={person.age}
+                      click = {() =>this.deletePersonHandler(index)}
+                      key ={person.id}
+                      changed = {(event) => this.nameChangedHandler(event,person.id)}
+                    />
+          })}
+        </div>
+      );
+      // Style.backgroundColor = 'red';
+      // Style[':hover'] = {
+      //   backgroundColor : 'salmon',
+      //   color : 'black'
+      // }
     }
+
+    const classes = [];
+    if(this.state.person.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.person.length <= 1){
+      classes.push('bold');
+    }
+
     return (
+ 
       <div className="App">
-      <h1>My React App</h1>
-        <p>{this.state.textLength}</p>
-        <input type="text" onChange={this.textChangeHandler}/>
-        <Validation mylength={this.state.textLength}/>
-        {character}
+       <h1>Hi, I'm a react app</h1>
+       <p className={classes.join(' ')}>This is really working!!</p>
+       <StyledButton alt={this.state.showPerson} onClick={this.togglePersonHandler}>Toggle and delete</StyledButton>
+        {persons}
       </div>
+     
     );
   }
 }
