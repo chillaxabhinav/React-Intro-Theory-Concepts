68f2c97582829512c2582927d59fc00abb7402b7
abhinav7693ngh <abhinav7693ngh@gmail.com>
2019-12-16 02:25:16 +0530

    Passing props to our HOC if our wrapped components uses props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,19 @@
-import React , {Fragment} from 'react';
-//import classes from './Person.css';
-//import Aux from '../../../Auxiliary/Auxiliary';
+import React , {Fragment , PureComponent} from 'react';
+import classes from './Person.css';
+import Aux from '../../../Auxiliary/Auxiliary';
+import withClass from '../../../Auxiliary/withClass';
 
 
-const person = (props) => {
-    return (
-        <Fragment>
-        <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old.</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-        </Fragment>
-    )
-};
+class Person extends PureComponent {
+    render(){
+        return (
+            <Aux>
+                <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </Aux>
+        )       
+    }
+}
 
-export default person;
+export default withClass(Person,classes.Person);
