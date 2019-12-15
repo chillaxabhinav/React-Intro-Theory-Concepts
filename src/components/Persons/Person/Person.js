0e92db2e7f92c1997f34aa9fd825c4ff13a6c06d
abhinav7693ngh <abhinav7693ngh@gmail.com>
2019-12-16 00:20:21 +0530

    Auxiliary HOC and Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import classes from './Person.css';
-
+import React , {Fragment} from 'react';
+//import classes from './Person.css';
+//import Aux from '../../../Auxiliary/Auxiliary';
 
 
 const person = (props) => {
     return (
-        <div className={classes.Person}>
+        <Fragment>
         <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old.</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
+        </Fragment>
     )
 };
 
