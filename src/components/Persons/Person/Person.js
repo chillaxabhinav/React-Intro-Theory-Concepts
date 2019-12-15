3b35708eeca234dd211919f0b062da6e4f6a94a0
abhinav7693ngh <abhinav7693ngh@gmail.com>
2019-12-16 04:35:39 +0530

    ref and React.createRef()

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,12 +6,27 @@ import PropTypes from 'prop-types';
 
 
 class Person extends PureComponent {
+
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount(){
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    };
     render(){
         return (
             <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input
+                    //ref = {(inputEl) => {this.inputElement = inputEl}}
+                    ref = {this.inputElementRef}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name} />
             </Aux>
         )       
     }
