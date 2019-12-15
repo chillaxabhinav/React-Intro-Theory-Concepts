cb6e48143f20f66962550fa8669418e51140d178
abhinav7693ngh <abhinav7693ngh@gmail.com>
2019-12-15 17:09:58 +0530

    redundant code removal

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import cockpit from '../components/Cockpit/Cockpit';
 // import ErrorBounday from './ErrorBoundary/ErrorBoundary';
 
 
